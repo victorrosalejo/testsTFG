6b65271f31112cf0a4f03fb302955e263de899a8
victorrosalejo <v.arroyom.2021@alumnos.urjc.es>
2025-06-30 13:20:45 +0200

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,3 @@
+export default function hola(nombre) {
+  return `Hola, ${nombre}!`;
+}
---
+++ src/rollup.config.js
@@ -0,0 +1,6 @@
+export default {
+  input: 'src/index.js',
+  output: [
+    { file: 'dist/mi-widget.esm.js', format: 'esm' }
+  ]
+}
